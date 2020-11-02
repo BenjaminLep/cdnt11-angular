f38fb87e596419c70913d8acd5dcf812a66fefcd
opusidea <opusidea@gmail.com>
2020-11-02 12:38:34 +0000

    start

---
+++ intro/src/app/header/header.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  title: string = "header component";
+  altText: string = "Photo d'un loup";
+  srcPath: string = "https://france3-regions.francetvinfo.fr/image/-Kvvx7U_RGF4WmdtOG-_xVPSXcw/930x620/regions/2020/06/09/5edf9ea708eb3_loup_10-4621851.jpg";
+  authenticated: boolean = false;
+  options: string[] = ["red", "green", "orange"];
+  borderColor: string = "black";
+
+
+  constructor() { 
+    this.updateUI();
+  }
+
+  updateUI() {
+    setTimeout(() => {
+      console.log("Update UI");
+      this.title += " updated !";
+      this.authenticated = !this.authenticated;
+      this.options.push("New option");
+    }, 1000)
+  }
+
+  onChange(event: any) {
+    let value: string = event.target.value;
+    console.log(value);
+
+    if (value === "New option") {
+      this.borderColor = "#ff6633";
+    } else {
+      this.borderColor = value;
+    }
+
+  }
+
+}
