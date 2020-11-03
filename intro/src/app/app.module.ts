978294f835674e585ce4624012141947b3fc2422
opusidea <opusidea@gmail.com>
2020-11-03 09:46:05 +0000

    J2 TP correction

--- intro/src/app/app.module.ts
+++ intro/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SwitchComponent } from './switch/switch.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { TestComponent } from './test/test.component';
+import { VirtualkbComponent } from './virtualkb/virtualkb.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SwitchComponent,
+    CarouselComponent,
+    TestComponent,
+    VirtualkbComponent
   ],
   imports: [
     BrowserModule
---
+++ intro/src/app/carousel/carousel.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+const FRUITS_DIR: string = "assets/images/fruits/";
+const SPEED: number = 2000; // milliseconds
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css']
+})
+export class CarouselComponent implements OnInit {
+  images: string[] = ["cerise.jpg", "pomme.jpg", "kiwi.jpg"];
+  loadedImage: string = "";
+  interval: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.startCarousel();
+  }
+
+  startCarousel() {
+    let i: number = 0;
+    this.interval = setInterval(() => {
+      if (i >= this.images.length) i = 0; // reset de i
+      this.loadedImage = FRUITS_DIR + this.images[i];
+      i++;
+    }, SPEED) 
+  }
+
+  stopCarousel() {
+    clearInterval(this.interval);
+  }
+}
